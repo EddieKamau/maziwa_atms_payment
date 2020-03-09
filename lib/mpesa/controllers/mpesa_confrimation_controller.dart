7cf5383b5e9a19cc2afec7adfa884fb1c080bbbc
EddieKamau <elyudde@gmail.com>
2020-03-09 12:57:14 +0300

    mpesa update

--- lib/mpesa/controllers/mpesa_confrimation_controller.dart
+++ lib/mpesa/controllers/mpesa_confrimation_controller.dart
@@ -1,5 +1,7 @@
 import 'package:maziwa_otp/maziwa_otp.dart';
 import 'package:maziwa_otp/mpesa/models/mpesa_models.dart' show MpesaConfirmationModel;
+import 'package:maziwa_otp/otp/models/otp_models.dart' show OtpModel;
+import 'package:maziwa_otp/sms/modules/sms_modules.dart' show SmsModule;
 
 class MpesaConfirmationController extends ResourceController{
 
@@ -10,12 +12,29 @@ class MpesaConfirmationController extends ResourceController{
     final MpesaConfirmationModel _mpesaConfirmationModel = MpesaConfirmationModel(body: _body);
     await _mpesaConfirmationModel.save();
 
-    // final String _amount = _body['TransAmount'].toString();
+    final String _amount = _body['TransAmount'].toString();
     // final String _businessShortCode = _body['BusinessShortCode'].toString();
-    // final String _phoneNo = _body['MSISDN'].toString();
-    // final String _transID = _body['TransID'].toString();
+    final String _phoneNo = _body['MSISDN'].toString();
+    final String _transID = _body['TransID'].toString();
+
+    // generate opt
+    final OtpModel otpModel = OtpModel(
+      refNo: _phoneNo.substring(8)
+    );
+    await otpModel.save();
 
     // send otp
+    final SmsModule smsModule = SmsModule(
+      phoneNo: _phoneNo,
+      body: '''
+      Mpesa transaction $_transID accepted for milk worth Ksh.$_amount
+      \nPayment reference is: ${otpModel.refNo}. \nOTP is: ${otpModel.otp}
+      \nThis OTP expires after 30 minutes.
+      '''
+    );
+    // await smsModule.jarvisSendSms();
+
+    await smsModule.save();
 
     return Response.ok({
       "ResultCode": 0,
